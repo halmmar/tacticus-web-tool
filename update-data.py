2a2482394714ccd2ff472fc9c25f157f95eb8439
Halmmar <halmmar@users.noreply.github.com>
2023-03-14 15:21:09 +0100

    Read Eldryon passive data from the spreadsheet

--- update-data.py
+++ update-data.py
@@ -65,6 +65,18 @@ file = 'EN Labs T.A.C.T.I.C.U.S - Beta 0.0.3.3.xlsx'
 tb_Characters = data_frame_from_xlsx(file, 'tb_Characters', 1)
 tb_Pierce = data_frame_from_xlsx(file, 'UL_Tables!$W$3:$X$21', True)
 tb_Gear = data_frame_from_xlsx(file, 'UL_Tables!$I$4:$M$21', True)
+tb_Abilities = data_frame_from_xlsx(file, 'wb_abilities!$AB$3:$AD$52', True)
+
+tb_Abilities = data_frame_from_xlsx(file, 'wb_abilities!$AB$3:$AD$52', True)
+tb_Abilities_1_50 = [level for (level,growth,factor) in tb_Abilities.itertuples(index=False)]
+for i in range(1,51):
+    if tb_Abilities_1_50[i-1] != i:
+        raise Exception("Did not find the correct table. i=%d, tb_Abilities=%d" % ( i, tb_Abilities_1_50[i-1]))
+tb_Abilities_factor = [factor for (level,growth,factor) in tb_Abilities.itertuples(index=False)]
+
+tb_CharacterAbilities = data_frame_from_xlsx(file, 'wb_abilities!$A$4:$U$51', True)
+characterAbilities = dict([(tpl[0],tpl[1:]) for tpl in tb_CharacterAbilities.itertuples(index=False)])
+
 pierce = dict([(k.lower(),v) for (k,v) in tb_Pierce.itertuples(index=False)])
 gear = [(rank,int(gearLevel)) for (rarity,rank,rankMetal,rankLevel,gearLevel) in tb_Gear.itertuples(index=False)]
 
@@ -73,12 +85,13 @@ def toJSON(rows):
     bosses = {}
     summons = {}
     for index, row in rows.iterrows():
-        if row.Name is None or np.isnan(row["Melee Hits"]) or np.isnan(row.Health):
+        if row.Name is None or np.isnan(row["Melee Hits"]) or np.isnan(row.Health) or row["Melee Damage"]==0:
             continue
-        data = {"faction": row.Faction, "health": row.Health, "damage": row.Damage, "armour": row.Armour, "melee": {"pierce": pierce[row["Melee Damage"].lower()], "hits": int(row["Melee Hits"])}}
+        data = {"faction": row.Faction, "alliance": row.Alliance, "health": row.Health, "damage": row.Damage, "armour": row.Armour, "melee": {"pierce": pierce[row["Melee Damage"].lower()], "hits": int(row["Melee Hits"])}}
         if row["Ranged Hits"] > 0:
             data["ranged"] = {"pierce": pierce[row["Ranged Damage"].lower()], "hits": int(row["Ranged Hits"])}
         if row["Initial rarity"] in ["Common", "Uncommon", "Rare", "Epic", "Legendary"]:
+            data["passive"] = [x for x in characterAbilities[row.Name][17:20] if not np.isnan(x)]
             characters[row.Name] = data
         elif row.Name[0:2] in ["L1", "L2", "L3", "L4"]:
             bosses[row.Name] = data
@@ -86,6 +99,6 @@ def toJSON(rows):
             summons[row.Name] = data
         else:
             continue
-    return {"characters": characters, "bosses": bosses, "summons": summons, "gear": gear, "version": file.split("-")[1].strip().replace(".xlsx", "")}
+    return {"characters": characters, "bosses": bosses, "summons": summons, "gear": gear, "abilities_factor": tb_Abilities_factor, "version": file.split("-")[1].strip().replace(".xlsx", "")}
 with open("tacticus.json", "w") as fout:
-    fout.write(json.dumps(toJSON(tb_Characters), indent=2))
+    fout.write(json.dumps(toJSON(tb_Characters), sort_keys=True, indent=2))
