2a7ec255530737a3149fd34d4c048de71c934c83
Halmmar <halmmar@users.noreply.github.com>
2023-03-15 10:42:10 +0100

    Implemented equipment and crits

--- update-data.py
+++ update-data.py
@@ -6,6 +6,7 @@ import xlsxwriter
 import subprocess
 import openpyxl
 import json
+from collections import OrderedDict 
 
 def data_frame_from_xlsx(xlsx_file, range_name, headerColumn=None):
     """ Get a single rectangular region from the specified file.
@@ -19,7 +20,7 @@ def data_frame_from_xlsx(xlsx_file, range_name, headerColumn=None):
         if ws_name.startswith("'") and ws_name.endswith("'"):
             # optionally strip single quotes around sheet name
             ws_name = ws_name[1:-1]
-        region = wb[ws_name][reg]
+        ws = wb[ws_name]
     else:
         # passed a named range; find the cells in the workbook
         full_range = wb.defined_names[range_name]
@@ -42,20 +43,20 @@ def data_frame_from_xlsx(xlsx_file, range_name, headerColumn=None):
         # but 2.4+ returns the name of a worksheet)
         if isinstance(ws, str):
             ws = wb[ws]
-        if headerColumn is None:
-            pass
+    region = ws[reg]
+    if headerColumn is None:
+        pass
+    else:
+        heads = reg.split("$")
+        if type(headerColumn) is int:
+            headRow = headerColumn
+        elif type(headerColumn) is bool:
+            headRow = int(heads[2][:-1])-1
         else:
-            heads = reg.split("$")
-            if type(headerColumn) is int:
-                headRow = headerColumn
-            elif type(headerColumn) is bool:
-                headRow = int(heads[2][:-1])-1
-            else:
-                raise Exception("...")
-            headRange = "$%s$%d:$%s$%d" % (heads[1],headRow,heads[3],headRow)
-            headRegion = ws[headRange]
-            columns = [cell.value.strip() for cell in headRegion[0]]
-        region = ws[reg]
+            raise Exception("...")
+        headRange = "$%s$%d:$%s$%d" % (heads[1],headRow,heads[3],headRow)
+        headRegion = ws[headRange]
+        columns = [(cell.value.strip() if isinstance(cell.value,str) else cell.value) for cell in headRegion[0]]
     # an anonymous user suggested this to catch a single-cell range (untested):
     # if not isinstance(region, 'tuple'): df = pd.DataFrame(region.value)
     df = pd.DataFrame(([cell.value for cell in row] for row in region), columns=columns)
@@ -66,6 +67,65 @@ tb_Characters = data_frame_from_xlsx(file, 'tb_Characters', 1)
 tb_Pierce = data_frame_from_xlsx(file, 'UL_Tables!$W$3:$X$21', True)
 tb_Gear = data_frame_from_xlsx(file, 'UL_Tables!$I$4:$M$21', True)
 tb_Abilities = data_frame_from_xlsx(file, 'wb_abilities!$AB$3:$AD$52', True)
+tb_Equipment = data_frame_from_xlsx(file, 'Equipment!$B$3:$AP$124', 2)
+factionMap = {
+    "ULTRAMARINES": "Ultramarines",
+    "ADEPTA SOROITAS": "ADEPTA SOROITAS",
+    "ASTRA MILITARUM": "Astra militarum",
+    "BLACK LEGION": "Black Legion",
+    "BLACK TEMPLARS": "Black templars",
+    "DEATH GUARD": "Death guard",
+    "ORKS": "Orks",
+    "NECRONS": "Necrons",
+    "AELDARI": "Aeldari",
+    "T'AU": "T'au Empire"
+}
+
+eqDict = {}
+for index, eq in tb_Equipment.iterrows():
+    if eq["Item Type"] is None:
+        continue
+    itemTypeStr = eq["Item Type"].lower().replace(" ","_")
+    if itemTypeStr not in eqDict:
+        eqDict[itemTypeStr] = {}
+    itemType = eqDict[itemTypeStr]
+    factions = []
+    for faction in factionMap.keys():
+        if eq[faction]:
+            factions += [factionMap[faction]]
+    itemInternalType = eq[0:1][0] # Gun, Knife, Both
+    chance = eq["Chance"]
+    if np.isnan(chance):
+        chance = int("Both"==itemInternalType)
+    else:
+        chance = int(chance)
+    if "booster" in itemTypeStr:
+        chance = 1
+    if chance in itemType:
+        itemType[chance]["factions"] = list(OrderedDict.fromkeys(itemType[chance]["factions"]+factions))
+    else:
+        itemType[chance] = {
+            "factions": factions
+        }
+    itemChance = itemType[chance]
+    rarityStr = eq["Rarity"].lower()
+    if rarityStr not in itemChance:
+        stat1 = [int(x) for x in eq[18:29] if not np.isnan(x)]
+        stat2 = [int(x) for x in eq[30:41] if not np.isnan(x)]
+        if itemTypeStr=="defense":
+            itemChance[rarityStr] = {
+                "health": stat1,
+                "armour": stat2
+            }
+        else:
+            itemChance[rarityStr] = {
+                {
+                    "crit": "crit-dmg",
+                    "crit_booster": "crit-dmg",
+                    "block": "block",
+                    "block_booster": "block"
+                }[itemTypeStr]: stat1
+            }
 
 tb_Abilities = data_frame_from_xlsx(file, 'wb_abilities!$AB$3:$AD$52', True)
 tb_Abilities_1_50 = [level for (level,growth,factor) in tb_Abilities.itertuples(index=False)]
@@ -87,11 +147,25 @@ def toJSON(rows):
     for index, row in rows.iterrows():
         if row.Name is None or np.isnan(row["Melee Hits"]) or np.isnan(row.Health) or row["Melee Damage"]==0:
             continue
+        eqCount = {"crit": 0, "defense": 0, "crit_booster": 0, "block": 0, "block_booster": 0}
+        for eqNum in ["Equipement slot 1", "Equipement slot 2", "Equipemnt slot 3"]:
+            if row[eqNum] is None or type(row[eqNum]) is not str:
+                continue
+            eq = row[eqNum].lower().replace(" ", "_")
+            if eq in eqCount:
+                eqCount[eq] += 1
+        toDelete = []
+        for key in eqCount.keys():
+            if not eqCount[key]:
+                toDelete += [key]
+        for key in toDelete:
+            del eqCount[key]
         data = {"faction": row.Faction, "alliance": row.Alliance, "health": row.Health, "damage": row.Damage, "armour": row.Armour, "melee": {"pierce": pierce[row["Melee Damage"].lower()], "hits": int(row["Melee Hits"])}}
         if row["Ranged Hits"] > 0:
             data["ranged"] = {"pierce": pierce[row["Ranged Damage"].lower()], "hits": int(row["Ranged Hits"])}
         if row["Initial rarity"] in ["Common", "Uncommon", "Rare", "Epic", "Legendary"]:
             data["passive"] = [x for x in characterAbilities[row.Name][17:20] if not np.isnan(x)]
+            data["equipment"] = eqCount
             characters[row.Name] = data
         elif row.Name[0:2] in ["L1", "L2", "L3", "L4"]:
             bosses[row.Name] = data
@@ -99,6 +173,6 @@ def toJSON(rows):
             summons[row.Name] = data
         else:
             continue
-    return {"characters": characters, "bosses": bosses, "summons": summons, "gear": gear, "abilities_factor": tb_Abilities_factor, "version": file.split("-")[1].strip().replace(".xlsx", "")}
+    return {"characters": characters, "bosses": bosses, "summons": summons, "gear": gear, "abilities_factor": tb_Abilities_factor, "equipment": eqDict, "version": file.split("-")[1].strip().replace(".xlsx", "")}
 with open("tacticus.json", "w") as fout:
     fout.write(json.dumps(toJSON(tb_Characters), sort_keys=True, indent=2))
